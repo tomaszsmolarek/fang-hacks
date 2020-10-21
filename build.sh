9bc77a37d2a50b595ca72eeaa1c54307828c13d5
Tomek Smolarek <tomek@rais.io>
2020-10-21 10:22:15 +0100

    quick & dirty - build script
    (cherry picked from commit f5c8a6ebd9542f5950f8b55c359231f57164526b)

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p releases/build_2.0.1-tomasz/vfat
+cp -r -f snx_autorun.sh releases/build_2.0.1-tomasz/vfat/
+cp -r -f bootstrap releases/build_2.0.1-tomasz/vfat/bootstrap/
+cp -f bootstrap/wpa_supplicant.conf.tomasz releases/build_2.0.1-tomasz/vfat/bootstrap/wpa_supplicant.conf
+
+mkdir -p releases/build_2.0.1-tomasz/ext2
+cp -r -f data releases/build_2.0.1-tomasz/ext2/data/
