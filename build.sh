26f2a73598a2b9537314d6abc274ddbed80a4a34
Tomek Smolarek <tomek@rais.io>
2020-10-21 16:07:44 +0100

    Adjusting build script

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-mkdir -p releases/build_2.0.1-tomasz/vfat
-cp -r -f snx_autorun.sh releases/build_2.0.1-tomasz/vfat/
-cp -r -f bootstrap releases/build_2.0.1-tomasz/vfat/bootstrap/
-cp -f bootstrap/wpa_supplicant.conf.tomasz releases/build_2.0.1-tomasz/vfat/bootstrap/wpa_supplicant.conf
+mkdir -p releases/build_2.0.1-tomasz2/vfat
+cp -r -f snx_autorun.sh releases/build_2.0.1-tomasz2/vfat/
+cp -r -f bootstrap releases/build_2.0.1-tomasz2/vfat/bootstrap/
+cp -f bootstrap/wpa_supplicant.conf.tomasz releases/build_2.0.1-tomasz2/vfat/bootstrap/wpa_supplicant.conf
 
-mkdir -p releases/build_2.0.1-tomasz/ext2
-cp -r -f data releases/build_2.0.1-tomasz/ext2/data/
-chown -R root releases/build_2.0.1-tomasz/ext2/data/
-chgrp -R root releases/build_2.0.1-tomasz/ext2/data/
+mkdir -p releases/build_2.0.1-tomasz2/ext2
+cp -r -f data releases/build_2.0.1-tomasz2/ext2/data/
+cp -f bootstrap/wpa_supplicant.conf.tomasz releases/build_2.0.1-tomasz2/ext2/data/etc/wpa_supplicant.conf
+chown -R root releases/build_2.0.1-tomasz2/ext2/data/
+chgrp -R root releases/build_2.0.1-tomasz2/ext2/data/
