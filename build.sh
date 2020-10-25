a2da8ff22be1d70fc6faedf1a9deccfe67f8d44b
Tomek Smolarek <tomek@rais.io>
2020-10-25 10:29:05 +0000

    Fix build script

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 mkdir -p releases/build_2.0.1-tomasz2/vfat
 cp -r -f snx_autorun.sh releases/build_2.0.1-tomasz2/vfat/
-cp -r -f bootstrap releases/build_2.0.1-tomasz2/vfat/bootstrap/
+cp -r -f bootstrap releases/build_2.0.1-tomasz2/vfat/
 cp -f bootstrap/wpa_supplicant.conf.tomasz releases/build_2.0.1-tomasz2/vfat/bootstrap/wpa_supplicant.conf
 cp -f bootstrap/rtsp.passwd.tomasz releases/build_2.0.1-tomasz2/vfat/bootstrap/rtsp.passwd
 
 mkdir -p releases/build_2.0.1-tomasz2/ext2
-cp -r -f data releases/build_2.0.1-tomasz2/ext2/data/
+cp -r -f data releases/build_2.0.1-tomasz2/ext2/
 cp -f bootstrap/wpa_supplicant.conf.tomasz releases/build_2.0.1-tomasz2/ext2/data/etc/wpa_supplicant.conf
 cp -f bootstrap/rtsp.passwd.tomasz releases/build_2.0.1-tomasz2/ext2/data/etc/rtsp.passwd
 chown -R root releases/build_2.0.1-tomasz2/ext2/data/
