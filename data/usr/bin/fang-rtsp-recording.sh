19aec387ec3e5b9d9894d3e8cbdaf4a1a9cbe9b7
Tomek Smolarek <tomek@rais.io>
2020-10-31 09:19:19 +0000

    Recording - add WIP folder

--- data/usr/bin/fang-rtsp-recording.sh
+++ data/usr/bin/fang-rtsp-recording.sh
@@ -3,23 +3,30 @@
 while :
 do
 	currentdt=`date -u '+%F_%H%M%S'_UTC`
+	log="/media/mmcblk0p2/data/recording_logs/$currentdt.log"
+	err_log="/media/mmcblk0p2/data/recording_logs/$currentdt.err"
+	record_to="/media/mmcblk0p2/data/recordings_wip/$currentdt.mkv"
+	move_to="/media/mmcblk0p2/data/recordings/$currentdt.mkv"
 
-	echo "Starting ffmpeg recording..." >> /media/mmcblk0p2/data/recording_logs/$currentdt.log
+	echo "$currentdt Starting ffmpeg recording..." >> $log
 
 	auth=$(cat /media/mmcblk0p2/data/etc/rtsp.passwd)
 	if [ "$auth" ]; then
-		echo "$currentdt Starting ffmpeg recording with auth" >> /media/mmcblk0p2/data/recording_logs/$currentdt.log
+		echo "$currentdt Starting ffmpeg recording with auth" >> $log
 	else
-		echo "$currentdt Starting ffmpeg recording without auth" >> /media/mmcblk0p2/data/recording_logs/$currentdt.log
+		echo "$currentdt Starting ffmpeg recording without auth" >> $log
 	fi
 
-	/media/mmcblk0p2/data/test/ffmpeg/ffmpeg -loglevel warning -i rtsp://$auth@127.0.0.1/unicast -vcodec copy -acodec copy -y -t 300 /media/mmcblk0p2/data/recordings/$currentdt.mkv > /media/mmcblk0p2/data/recording_logs/$currentdt.log 2> /media/mmcblk0p2/data/recording_logs/$currentdt.err
+	/media/mmcblk0p2/data/test/ffmpeg/ffmpeg -loglevel warning -i rtsp://$auth@127.0.0.1/unicast -vcodec copy -acodec copy -y -t 300 $record_to > $log 2> $err_log
 
 	if [ "$?" -ne "0" ]; 
 	then
-		echo "$currentdt Recording with ffmpeg finished with errors, will sleep 10" >> /media/mmcblk0p2/data/recording_logs/$currentdt.log
+		echo "$currentdt Recording with ffmpeg finished with errors, will sleep 10" >> $log
 		sleep 10
 	else
-		echo "$currentdt Recording with ffmpeg finished OK" >> /media/mmcblk0p2/data/recording_logs/$currentdt.log
+		echo "$currentdt Recording with ffmpeg finished OK" >> $log
 	fi
+
+	mv $record_to $move_to
+	echo "$currentdt Moved file from $record_to to $move_to" >> $log
 done
