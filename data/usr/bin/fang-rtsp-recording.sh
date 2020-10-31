292551dfa487db12cb9b0b0ec5719ae79f426767
Tomek Smolarek <tomek@rais.io>
2020-10-31 10:39:58 +0000

    Adjusting working on recording script

--- data/usr/bin/fang-rtsp-recording.sh
+++ data/usr/bin/fang-rtsp-recording.sh
@@ -3,21 +3,30 @@
 while :
 do
 	currentdt=`date -u '+%F_%H%M%S'_UTC`
-	log="/media/mmcblk0p2/data/recording_logs/$currentdt.log"
-	err_log="/media/mmcblk0p2/data/recording_logs/$currentdt.err"
-	record_to="/media/mmcblk0p2/data/recordings_wip/$currentdt.mkv"
-	move_to="/media/mmcblk0p2/data/recordings/$currentdt.mkv"
+	log="/media/mmcblk0p2/data/recordings_logs/$currentdt.log"
+	rec_log="/media/mmcblk0p2/data/recordings_logs/$currentdt-rec.log"
+	rec_err_log="/media/mmcblk0p2/data/recordings_logs/$currentdt-rec.err"
+	record_folder="/media/mmcblk0p2/data/recordings_wip/"
+	record_to="$record_folder$currentdt.mkv"
+	dest_folder="/media/mmcblk0p2/data/recordings/"
+	move_to="$dest_folder$currentdt.mkv"
 
-	echo "$currentdt Starting ffmpeg recording..." >> $log
+	touch $log
+	touch $rec_log
+	touch $rec_err_log
+
+	echo "$currentdt Will move all incomplete recordings from $record_folder to $dest_folder" >> $log 
+	tmp_move_src="$record_folder*"
+	mv $tmp_move_src $dest_folder
 
 	auth=$(cat /media/mmcblk0p2/data/etc/rtsp.passwd)
 	if [ "$auth" ]; then
 		echo "$currentdt Starting ffmpeg recording with auth" >> $log
+		/media/mmcblk0p2/data/test/ffmpeg/ffmpeg -loglevel warning -i rtsp://$auth@127.0.0.1/unicast -vcodec copy -acodec copy -y -t 300 $record_to >> $rec_log 2>> $rec_err_log
 	else
 		echo "$currentdt Starting ffmpeg recording without auth" >> $log
+		/media/mmcblk0p2/data/test/ffmpeg/ffmpeg -loglevel warning -i rtsp://127.0.0.1/unicast -vcodec copy -acodec copy -y -t 300 $record_to >> $rec_log 2>> $rec_err_log
 	fi
-
-	/media/mmcblk0p2/data/test/ffmpeg/ffmpeg -loglevel warning -i rtsp://$auth@127.0.0.1/unicast -vcodec copy -acodec copy -y -t 300 $record_to > $log 2> $err_log
 
 	if [ "$?" -ne "0" ]; 
 	then
@@ -29,4 +38,5 @@ do
 
 	mv $record_to $move_to
 	echo "$currentdt Moved file from $record_to to $move_to" >> $log
+
 done
