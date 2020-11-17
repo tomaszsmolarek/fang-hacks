96cabe9b477707b5e16e67e7216f1b935fc389b2
Tomek Smolarek <tomek@rais.io>
2020-11-17 13:59:39 +0000

    Adjusting rec script

--- data/usr/bin/fang-rtsp-recording.sh
+++ data/usr/bin/fang-rtsp-recording.sh
@@ -3,7 +3,9 @@
 while :
 do
 	CURRENTDT=`date -u '+%F_%H%M%S'_UTC`
-	LOGS_FOLDER="/media/mmcblk0p2/data/recordings_logs/"
+	CURRENTD=`date -u '+%F'_UTC`
+	LOGS_FOLDER_BASE="/media/mmcblk0p2/data/recordings_logs/"
+	LOGS_FOLDER="$LOGS_FOLDER_BASE$CURRENTD/"
 	LOG="$LOGS_FOLDER$CURRENTDT.log"
 	REC_LOG="$LOGS_FOLDER$CURRENTDT-rec.log"
 	REC_ERR_LOG="$LOGS_FOLDER$CURRENTDT-rec.err"
@@ -18,17 +20,25 @@ do
 	OLD_FILES_PURGE_DAYS=14
 	HDD_SPACE_THRESHOLD_KB=102400
 
+	echo "$CURRENTDT Will remove empty folders from $LOGS_FOLDER_BASE and $RECORD_FOLDER and $DEST_FOLDER" >> $LOG 
+	find $LOGS_FOLDER_BASE -type d -exec rmdir {} + 2>/dev/null
+	find $RECORD_FOLDER -type d -exec rmdir {} + 2>/dev/null
+	find $DEST_FOLDER -type d -exec rmdir {} + 2>/dev/null
+
+	mkdir -p $LOGS_FOLDER
+	mkdir -p $RECORD_FOLDER
+	mkdir -p $DEST_FOLDER
 	touch $LOG
 	touch $REC_LOG
 	touch $REC_ERR_LOG
 
 	echo "$CURRENTDT Will move all incomplete recordings from $RECORD_FOLDER to $DEST_FOLDER" >> $LOG 
 	tmp_move_src="$RECORD_FOLDER*"
-	mv $tmp_move_src $DEST_FOLDER >> $LOG 2>&1
+	mv $tmp_move_src $DEST_FOLDER >> $LOG 2>/dev/null
 
-	echo "$CURRENTDT Will remove files older than $OLD_FILES_PURGE_DAYS days from $LOGS_FOLDER" >> $LOG 
-	find $LOGS_FOLDER -mtime +$OLD_FILES_PURGE_DAYS -type f -print >> $LOG 2>&1
-	find $LOGS_FOLDER -mtime +$OLD_FILES_PURGE_DAYS -type f -exec rm {} \; >> $LOG 2>&1
+	echo "$CURRENTDT Will remove files older than $OLD_FILES_PURGE_DAYS days from $LOGS_FOLDER_BASE" >> $LOG 
+	find $LOGS_FOLDER_BASE -mtime +$OLD_FILES_PURGE_DAYS -type f -print >> $LOG 2>&1
+	find $LOGS_FOLDER_BASE -mtime +$OLD_FILES_PURGE_DAYS -type f -exec rm {} \; >> $LOG 2>&1
 
 	echo "$CURRENTDT Will remove files older than $OLD_FILES_PURGE_DAYS days from $DEST_FOLDER" >> $LOG 
 	find $DEST_FOLDER -mtime +$OLD_FILES_PURGE_DAYS -type f -print >> $LOG 2>&1
@@ -40,7 +50,7 @@ do
 		echo "$CURRENTDT There's not enough space on SD CARD! Free space is $FREE_SDCARD_SPACE_KB KB, need $HDD_SPACE_THRESHOLD_KB KB" >> $LOG
 		OLDEST_FILE=`ls -t $DEST_FOLDER | tail -1`
 		echo "$CURRENTDT Will delete oldest file ($DEST_FOLDER/$OLDEST_FILE) and continue recording" >> $LOG
-		rm $OLDEST_FILE
+		rm -f $DEST_FOLDER/$OLDEST_FILE >> $LOG 2>&1
 	fi
 
 	auth=$(cat /media/mmcblk0p2/data/etc/rtsp.passwd)
